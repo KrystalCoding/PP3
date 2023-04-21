94022aa23b623d576894ec2bfc19b11652f93c2b
KrystalCoding <>
2023-04-21 12:47:58 +0000

    Deploy to Heroku, and add more questions to plant quiz

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import random
 
-# Define a list of questions and answers
+# The list of questions and answers in the quiz game
 questions = [
     {
         "question": "What is the scientific name for the common sunflower?",
-        "answers": ["Helianthus annuus", "Rosa canina", "Cucurbita pepo", "Lilium auratum"],
+        "answers": ["Helianthus annuus", "Rosa rubiginosa", "Cucurbita pepo", "Lilium auratum"],
         "correct_answer": "Helianthus annuus"
     },
     {
@@ -17,10 +17,59 @@ questions = [
         "answers": ["Vine", "Shrub", "Tree", "Grass"],
         "correct_answer": "Vine"
     },
-    # Add more questions here...
+    {
+        "question": "What is the process called by which plants convert light into energy?",
+        "answers": ["Photosynthesis", "Respiration", "Transpiration", "Fertilization"],
+        "correct_answer": "Photosynthesis"
+    },
+    {
+        "question": "What is the largest flower in the world?",
+        "answers": ["Rafflesia arnoldii", "Titan arum", "Corpse flower", "Hydrangea macrophylla"],
+        "correct_answer": "Rafflesia arnoldii"
+    },
+    {
+        "question": "What is the most common element found in all living organisms?",
+        "answers": ["Carbon", "Oxygen", "Nitrogen", "Hydrogen"],
+        "correct_answer": "Carbon"
+    },
+    {
+        "question": "What is the name of the largest tree in the world?",
+        "answers": ["Sequoia sempervirens", "Eucalyptus regnans", "Picea sitchensis", "Sequoiadendron giganteum"],
+        "correct_answer": "Sequoiadendron giganteum"
+    },
+    {
+        "question": "What is the process called by which plants lose water through their leaves?",
+        "answers": ["Transpiration", "Photosynthesis", "Respiration", "Fertilization"],
+        "correct_answer": "Transpiration"
+    },
+    {
+        "question": "What is the national flower of Japan?",
+        "answers": ["Cherry blossom", "Lotus", "Rose", "Tulip"],
+        "correct_answer": "Cherry blossom"
+    },
+    {
+        "question": "What is the name of the plant tissue that transports water and nutrients from the roots to the rest of the plant?",
+        "answers": ["Xylem", "Phloem", "Cambium", "Epidermis"],
+        "correct_answer": "Xylem"
+    },
+    {
+        "question": "What is the name of the process by which plants bend or grow towards a light source?",
+        "answers": ["Phototropism", "Gravitropism", "Hydrotropism", "Thigmotropism"],
+        "correct_answer": "Phototropism"
+    },
+    {
+        "question": "What is the name of the process by which plants shed their leaves?",
+        "answers": ["Abcission", "Transpiration", "Photosynthesis", "Respiration"],
+        "correct_answer": "Abcission"
+    },
+    {
+        "question": "What is the name of the chemical compound responsible for giving plants their green color?",
+        "answers": ["Chlorophyll", "Carotenoid", "Anthocyanin", "Chloroform"],
+        "corect_answer": "Chlorophyll"
+    }    
 ]
 
-# Define a function to run the game
+# Defines the function to run the game
 def run_game():
     random.shuffle(questions)  # Shuffle the questions
     score = 0  # Initialize the score
